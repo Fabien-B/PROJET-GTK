e503429b9b53f7135136c13b7d632ca8ac16c6cb
Fabien-B <fabien.bonneval@gmail.com>
2014-06-18 07:39:27 +0200

    suppression des projets exemple, ajout de petits mémos, et corrections mineures pour rendre le code pus propre.

--- Exemples_code/Correction_TP/callbacks.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALLBACKS_H
-#define CALLBACKS_H
-#include "donnees.h"
-#include <gtk/gtk.h>
-
-void new_game(GtkWidget *widget, donnees *d);	// une nouvelle partie
-void joue(GtkWidget *widget, donnees *d);	// un bouton a ete enfonce
-
-#endif
