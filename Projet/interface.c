56bc6234957040ba8cbbea016486c802401cd794
Fabien-B <fabien.bonneval@gmail.com>
2014-06-20 10:53:27 +0200

    ajout de la licence, avec modif du "À Propos"

--- Projet/interface.c
+++ Projet/interface.c
@@ -425,10 +425,12 @@ redessiner(NULL,donnees->carte);
 void APropos(GtkWidget* widget)
 {
     GtkWidget* APropos_box;
-    APropos_box = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "The GATI, 'The GNU Air Traffic Indicator' est un software devellopé dans le cadre d'un projet étudiant.");
+    APropos_box = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "The GATI, 'The GNU Air Traffic Indicator' est un software devellopé dans le cadre d'un projet étudiant.\nCopyright (C) 2014  Fabien Bonneval, Xavier Durand.\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\nYou should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.");
+
     gtk_dialog_run(GTK_DIALOG(APropos_box));
     gtk_widget_destroy(APropos_box);
 }
+
 
 void voir_pdv(GtkWidget *bouton, file_opener* donnees)
 {
